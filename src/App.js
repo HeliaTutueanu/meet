9a9c9722477500efb6c79cbc291a69892194d49b
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2024-03-01 17:37:36 +0200

    added rechart+grid+updated styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css';
 
-const App = () => {
-  const [allLocations, setAllLocations] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
+function App() {
   const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
   const [warningAlert, setWarningAlert] = useState("");
-  
+
   useEffect(() => {
     if (navigator.onLine) {
       setWarningAlert("");
@@ -27,24 +29,38 @@ const App = () => {
   const fetchData = async () => {
     const allEvents = await getEvents();
 
-    const filteredEvents = currentCity === "See all cities" ?
+    let filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
-    
+
+    if (currentNOE && currentNOE < filteredEvents.length) {
+      filteredEvents = filteredEvents.slice(0, currentNOE)
+    }
     setEvents(filteredEvents.slice(0, currentNOE));
+
     setAllLocations(extractLocations(allEvents));
   }
-
+  
   return (
     <div className="App">
+      <h1>Meet App</h1>
+      <p>Choose your nearest city</p>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
         {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE}  setErrorAlert={setErrorAlert} />
-      <EventList events={events} />
+      <CitySearch 
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
+      <div className='charts-container'>
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events}/>
+      </div>
+      <EventList events={events}/>
     </div>
   );
 }
